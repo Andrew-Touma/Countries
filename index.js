99e1230b22cfd5792bb1a4f4396c15b60c07b4a1
Andrew Touma <andrew.touma4000@hotmail.com>
2025-10-17 19:05:24 +0300

    Body style center changed button and selection style sorted the countries and showed the flag of selected country per selection

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-let countries=[];
+let countries = [];
 
-function handleSelection(){
+function handleSelection() {
     let ddl_countries = document.getElementById('ddl_countries');
     let selected_country = ddl_countries.value;
+    let div_flags = document.getElementById('div_flags')
+    div_flags.innerHTML = '';
 
     fetch(`https://restcountries.com/v3.1/name/${selected_country}`)
         .then((response) => response.json())
@@ -10,17 +12,22 @@ function handleSelection(){
             let country_info = document.getElementById('country_info');
             let capital = json[0].capital[0];
             let population = json[0].population;
+            country_info.className = "Info"
             country_info.innerHTML = `The capital of ${json[0].name.common} is : ${capital}.
             ${json[0].name.common} has a population equal to ${population}`;
-            // printing population
+
+            let img_flag = document.createElement('img');
+            img_flag.src = json[0].flags.png;
+            img_flag.className = 'ClassFlag';
+            div_flags.appendChild(img_flag);
         });
 }
-function ConsumeAPIData(){
+function ConsumeAPIData() {
     fetch('https://restcountries.com/v3.1/all?fields=name,flags')
         .then((response) => response.json())
         .then((json) => {
             json.forEach(element => {
-                countries.push( {
+                countries.push({
                     name: element.name.common,
                     flag: element.flags.png,
                 });
@@ -29,23 +36,28 @@ function ConsumeAPIData(){
 }
 ConsumeAPIData();
 
-function loadCountries()
-{
+function loadCountries() {
     let ddl_countries = document.getElementById('ddl_countries');
     let div_flags = document.getElementById('div_flags');
-
-   countries.forEach((x) => {
-    let option = document.createElement('option');
-    option.innerHTML = x.name;
-    ddl_countries.appendChild(option);
-
-    let img_flag = document.createElement('img');
-    img_flag.src = x.flag;
-    img_flag.className ='ClassFlag';
-    img_flag.addEventListener('click', () => {
-        alert(x.name);
+    let country_info = document.getElementById('country_info');
+    country_info.innerHTML = '';
+    div_flags.innerHTML = '';
+    countries.sort((a, b) => {
+        return a.name.localeCompare(b.name);
     })
+
+    countries.forEach((x) => {
+        let option = document.createElement('option');
+        option.innerHTML = x.name;
+        ddl_countries.appendChild(option);
+
+        let img_flag = document.createElement('img');
+        img_flag.src = x.flag;
+        img_flag.className = 'ClassFlag';
+        img_flag.addEventListener('click', () => {
+            alert(x.name);
+        })
         div_flags.appendChild(img_flag);
-   })
+    })
 
 }
